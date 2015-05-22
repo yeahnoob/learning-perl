5cc8aa841a8f00e475b5f2d5eeb6c69d2b317158
Xanadu Zhang <yeahnoob@buaa.edu.cn>
2015-05-22 16:39:29 +0800

    use private RSA key to get crypt hash URL

--- qrcode-ex/ex_1.pl
+++ qrcode-ex/ex_1.pl
@@ -12,9 +12,16 @@ open my $frh, "<", $ARGV[0];
 
 my $d = Crypt::Digest::SHA256->new;
 $d->addfile($frh);    # input the document
-my $dhash = join '',
-  ( "http://192.168.0.120:8080/doc/check?id=", $d->hexdigest );
-print $dhash, "\n";
+my $dhash = $d->hexdigest;
+print "Document's Yaml File => Hash : ", $dhash, "\n";
+my $crypt_d = Crypt::Digest::SHA256->new;
+$crypt_d->addfile($frh);  # the application document's file name
+$crypt_d->addfile("dm_rsa.priv.pem");  # the server's private RSA key.
+my $crypt_dhash = $crypt_d->hexdigest;
+print "Hash => Crypt_Hash : ", $crypt_dhash, "\n";
+
+my $qrcode_str = join '',
+  ( "http://192.168.0.120:8080/doc/check?id=", $crypt_dhash);
 my $qrcode = Imager::QRCode->new(
     size          => 4,
     margin        => 3,
@@ -25,7 +32,7 @@ my $qrcode = Imager::QRCode->new(
     lightcolor    => Imager::Color->new( 255, 255, 255 ),
     darkcolor     => Imager::Color->new( 0, 0, 0 ),
 );
-my $img = $qrcode->plot($dhash);
+my $img = $qrcode->plot($qrcode_str);
 $img->write( file => "output.qrcode.png" );
 
 # no more
